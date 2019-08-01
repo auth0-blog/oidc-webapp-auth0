d92b80d7aa48f4d10373d6a1a099db7c16978224
Bruno Krebs <bruno.krebs@auth0.com>
2019-07-31 23:29:31 -0300

    implementing web app sign in with SDK

--- src/server.js
+++ src/server.js
@@ -13,6 +13,21 @@ require('dotenv').config();
 
 const app = express();
 
+// Configure Passport to use Auth0
+const auth0Strategy = new Auth0Strategy(
+  {
+    domain: process.env.OIDC_PROVIDER,
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'http://localhost:3000/callback'
+  },
+  (accessToken, refreshToken, extraParams, profile, done) => {
+    profile.idToken = extraParams.id_token;
+    return done(null, profile);
+  }
+);
+passport.use(auth0Strategy);
+
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
 app.use(passport.initialize());
@@ -42,12 +57,22 @@ app.get('/profile', (req, res) => {
   });
 });
 
-app.get('/login', (req, res) => {
-  res.status(501).send();
-});
+app.get(
+  '/login',
+  passport.authenticate('auth0', {
+    scope: 'openid email profile'
+  })
+);
 
-app.post('/callback', async (req, res) => {
-  res.status(501).send();
+app.get('/callback', (req, res, next) => {
+  passport.authenticate('auth0', (err, user) => {
+    if (err) return next(err);
+    if (!user) return res.redirect('/login');
+    req.logIn(user, function(err) {
+      if (err) return next(err);
+      res.redirect('/profile');
+    });
+  })(req, res, next);
 });
 
 app.listen(3000, () => {
